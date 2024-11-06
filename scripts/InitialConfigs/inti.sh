0168c7381be8129e18a78c341bf0a8671c009f24
Baedroch <125892508+Baedroch@users.noreply.github.com>
2024-11-06 21:29:51 +0000

    added proxmox scripting directory, configs directory, init config

---
+++ scripts/InitialConfigs/inti.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# This script runs with the assumption of sudo priv
+# Define the new content to be added
+new_content="# Not for production use:
+deb http://download.proxmox.debian buster pve-no-subscription"
+# Define the file path for enterprise
+pve_file_path="/etc/apt/sources.list.d/pve-enterprise.list"
+ceph_file_path="/etc/apt/sources.list.d/ceph.list"
+# Remove any extra blank lines at the end of the file
+sed -i '/^$/d' /etc/apt/sources.list
+# Add a single blank line, then append the new content
+echo -e "\n$new_content" | tee -a /etc/apt/sources.list > /dev/null
+
+# Insert "#No subscription" above the line and comment out the "deb https..." line
+sudo sed -i '/^deb https:\/\/enterprise.proxmox.com\/debian\/pve bookworm pve-enterprise/ i #No subscription' "$pve_file_path"
+sudo sed -i 's|^deb https://enterprise.proxmox.com/debian/pve bookworm pve-enterprise|#&|' "$pve_file_path"
+
+# Insert "# disable enterprise" at the top and comment out the repository line
+sudo sed -i '1i # disable enterprise' "$ceph_file_path"
+sudo sed -i 's|^deb https://enterprise.proxmox.com/debian/ceph-quincy|#&|' "$ceph_file_path"
+
+# Update the system
+apt update -y
+apt upgrade -y
